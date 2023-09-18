daa5931899dc63b1b73b4b34a319ee3a01859be9
Salamatova <66477930+Salamatova@users.noreply.github.com>
2023-09-18 19:07:39 +0700

    Update streamlit_app.py
    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,37 @@
-import streamlit
+import streamlit as st
 import snowflake.connector
-streamlit.title("My parents New Healthy Diner")
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+# Set the Streamlit app title
+st.title("My Parent's New Healthy Diner")
 
+# Get the Snowflake secrets from Streamlit secrets
+snowflake_secrets = st.secrets["snowflake"]
 
+# Create a Snowflake connection using the secrets
+try:
+    conn = snowflake.connector.connect(
+        user=snowflake_secrets["user"],
+        password=snowflake_secrets["password"],
+        account=snowflake_secrets["account"],
+        warehouse=snowflake_secrets["warehouse"],
+        database=snowflake_secrets["database"],
+        schema=snowflake_secrets["schema"]
+    )
+
+    cursor = conn.cursor()
+
+    # Execute a Snowflake query
+    cursor.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+    data_row = cursor.fetchone()
+
+    # Display the Snowflake query result in Streamlit
+    st.text("Hello from Snowflake:")
+    st.text(data_row)
+
+except Exception as e:
+    st.error(f"Error: {str(e)}")
+
+finally:
+    # Close the Snowflake cursor and connection
+    cursor.close()
+    conn.close()
